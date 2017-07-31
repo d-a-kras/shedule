9640de88fba24049537a8c16f411b7f633cd7069
d-a-kras <d.a.kras@mail.ru>
2017-08-01 00:48:42 +0500

    1

--- shedule/Form1.Designer.cs
+++ shedule/Form1.Designer.cs
@@ -1473,8 +1473,8 @@
             this.Controls.Add(this.buttonTest);
             this.Controls.Add(this.labelStatus2);
             this.Controls.Add(this.labelStatus1);
-            this.Controls.Add(this.panelMultShops);
             this.Controls.Add(this.panelSingleShop);
+            this.Controls.Add(this.panelMultShops);
             this.HelpButton = true;
             this.MaximizeBox = false;
             this.Name = "Form1";
