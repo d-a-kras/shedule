fa413d2d4adb0afba7bebdfb28791b088333b824
d-a-kras <d.a.kras@mail.ru>
2017-08-09 23:13:19 +0500

    1

--- shedule/Code/Constants.cs
+++ shedule/Code/Constants.cs
@@ -10,6 +10,6 @@ namespace shedule.Code
     {
         public const string ListName = "График";
         public const string Version = "1.0";
-        public const string ReleaseDate = "4.08.2017";
+        public const string ReleaseDate = "8.08.2017";
     }
 }
