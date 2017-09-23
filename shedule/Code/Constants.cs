f7d0ca6643d60de59291a0796a21376370221772
destroyer25t <js7ussr@gmail.com>
2017-09-23 14:40:45 +0500

    -Версия от 23.09

--- shedule/Code/Constants.cs
+++ shedule/Code/Constants.cs
@@ -9,8 +9,8 @@ namespace shedule.Code
     public static class Constants
     {
         public const string ListName = "График";
-        public const string Version = "1.0";
-        public const string ReleaseDate = "12.09.2017";
+        public const string Version = "1.1";
+        public const string ReleaseDate = "23.09.2017";
         public const bool IsThrowExceptionOnNullResult = true; //будет ли программа порождать исключение если из базы вернулось 0 результатов
     }
 }
